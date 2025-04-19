4afabb350b769d04771a70e9352d5ca184d1f60a
Ann Nechytailenko <anechytailenko@kse.org.ua>
2025-04-19 20:01:09 +0300

    Update Program.cs

--- DNET.Backend.Api/Program.cs
+++ DNET.Backend.Api/Program.cs
@@ -93,11 +93,9 @@ builder.Services.AddHostedService<DeleteExpiredResetTokensService>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseMiddleware<ExceptionHandlerMiddleware>();
 
